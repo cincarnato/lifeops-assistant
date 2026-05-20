999c598a9e4449d89347a3ca557b11e8fdf49347
cincarnato <cristian.cdi@gmail.com>
2026-05-20 15:48:24 -0300

    fix: agent cine

---
+++ front/src/modules/lifeops/pages/AgentCinePage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import AgentCine from "@/modules/lifeops/components/AgentCine.vue";
+</script>
+
+<template>
+<agent-cine></agent-cine>
+</template>
+
+<style scoped>
+
+</style>
